d0084869769f43f8bc89b09426af0c8aa29d5729
MikePowar <mike.powar@gmail.com>
2019-06-21 20:09:50 -0700

    Add files via upload
    SideBar in progress

--- avotecado/imports/ui/Navbar.jsx
+++ avotecado/imports/ui/Navbar.jsx
@@ -4,26 +4,150 @@ import { AppBar, Toolbar, Button, IconButton } from '@material-ui/core';
 import TypoGraphy from '@material-ui/core/Typography'
 import MenuIcon from '@material-ui/icons/Menu';
 import { makeStyles } from '@material-ui/core/styles';
+//New imports from mui sample Dashboard
+import clsx from 'clsx';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import Badge from '@material-ui/core/Badge';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Link from '@material-ui/core/Link';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+
+import { PolListSideBarItems } from './PolListSideBar';
+
+//Will need to import the poli list like below:
+//import { mainListItems, secondaryListItems } from './listItems';
+
+
+
+// const useStyles = makeStyles(theme => ({
+// 	root: {
+// 		flexGrow: 1,
+// 	},
+// 	menuButton: {
+// 		marginRight: theme.spacing(2),
+// 	},
+// 	title: {
+// 		flexGrow: 1,
+// 	},
+// }));
+
+const drawerWidth = 240;
 
 const useStyles = makeStyles(theme => ({
-	root: {
-		flexGrow: 1,
-	},
-	menuButton: {
-		marginRight: theme.spacing(2),
-	},
-	title: {
-		flexGrow: 1,
-	},
+  root: {
+    display: 'flex',
+  },
+  toolbar: {
+    paddingRight: 24, // keep right padding when drawer closed
+  },
+  toolbarIcon: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: '0 8px',
+    ...theme.mixins.toolbar,
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: 36,
+  },
+  menuButtonHidden: {
+    display: 'none',
+  },
+  title: {
+    flexGrow: 1,
+  },
+  drawerPaper: {
+    position: 'relative',
+    whiteSpace: 'nowrap',
+    width: drawerWidth,
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  drawerPaperClose: {
+    overflowX: 'hidden',
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    width: theme.spacing(7),
+    [theme.breakpoints.up('sm')]: {
+      width: theme.spacing(9),
+    },
+  },
+  appBarSpacer: theme.mixins.toolbar,
+  content: {
+    flexGrow: 1,
+    height: '100vh',
+    overflow: 'auto',
+  },
+  container: {
+    paddingTop: theme.spacing(4),
+    paddingBottom: theme.spacing(4),
+  },
+  paper: {
+    padding: theme.spacing(2),
+    display: 'flex',
+    overflow: 'auto',
+    flexDirection: 'column',
+  },
+  fixedHeight: {
+    height: 240,
+  },
 }));
+
+
 
 export default function NavBar() {
 	const classes = useStyles();
+	//New imports from mui sample Dashboard
+	const [open, setOpen] = React.useState(true);
+	const handleDrawerOpen = () => {
+	  setOpen(true);
+	};
+	const handleDrawerClose = () => {
+	  setOpen(false);
+	};
+	const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
+
 	return (
 		<div className={classes.root}>
-			<AppBar color="primary" position="static">
-				<Toolbar>
-					<IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="Menu">
+			<AppBar color="primary" position="static" 
+				className={clsx(classes.appBar, open && classes.appBarShift)}>
+			
+			{/* // New imports from mui sample Dashboard: */}
+
+				<Toolbar className={classes.toolbar}>
+					<IconButton
+						edge="start" 
+						className={classes.menuButton} 
+						color="inherit" 
+						aria-label="Open drawer"
+						onClick={handleDrawerOpen}
+						className={clsx(classes.menuButton, open && classes.menuButtonHidden)}
+						>
 						<MenuIcon />
 					</IconButton>
 					<TypoGraphy variant="h5" color="inherit">
@@ -31,26 +155,34 @@ export default function NavBar() {
 			   </TypoGraphy>
 				</Toolbar>
 			</ AppBar>
+
+			{/* // New imports from mui sample Dashboard: */}
+
+			<Drawer
+        		variant="permanent"
+        		classes={{
+          		paper: clsx(classes.drawerPaper, !open && classes.drawerPaperClose),
+				}}
+				open={open}
+      		>
+        
+			<div className={classes.toolbarIcon}>
+          		<IconButton onClick={handleDrawerClose}>
+            		<ChevronLeftIcon />
+          		</IconButton>
+			  </div>
+			  <Divider />
+			  {/*  // TODO: Need to adjust below with PoliList */}
+			  	<List>
+					  
+					  { PolListSideBarItems }
+					  
+				</List> }
+        		<Divider />
+
+
+			</Drawer>
 		</div>
 	);
 }
 
-// class Navbar extends Component {
-// 	render() {
-// 		let styling = {
-// 			backgroundColor: 'silver'
-// 		};
-// 		return (
-// 			<div id="navbar">
-// 		        <div id="menu_button">
-// 		            <img src="icons/ic_menu_48px.png" width="36" height="24" />
-// 		        </div>
-// 		        <div id="header_text">
-// 		            avotecado.
-// 		        </div>
-// 		    </div>
-// 		);
-// 	}
-// }
-
-// export default Navbar;
---
+++ avotecado/imports/ui/PolListSideBar.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText'
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListSubheader from '@material-ui/core/ListSubheader';
+import DashboardIcon from '@material-ui/icons/Dashboard';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import PeopleIcon from '@material-ui/icons/People';
+import BarChartIcon from '@material-ui/icons/BarChart';
+import LayersIcon from '@material-ui/icons/Layers';
+import AssignmentIcon from '@material-ui/icons/Assignment';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+import ImageIcon from '@material-ui/icons/Image';
+import WorkIcon from '@material-ui/icons/Work';
+import BeachAccessIcon from '@material-ui/icons/BeachAccess';
+
+// const useStyles = makeStyles(theme => ({
+//   root: {
+//     width: '100%',
+//     maxWidth: 360,
+//     backgroundColor: theme.palette.background.paper,
+//   },
+// }));
+
+// const classes = useStyles();
+
+export const PolListSideBarItems = (
+    <div>
+    <ListItem button>
+      <ListItemIcon>
+        <DashboardIcon />
+      </ListItemIcon>
+      <ListItemText primary="Dashboard" />
+    </ListItem>
+    <ListItem button>
+      <ListItemIcon>
+        <ShoppingCartIcon />
+      </ListItemIcon>
+      <ListItemText primary="Orders" />
+    </ListItem>
+    <ListItem button>
+      <ListItemIcon>
+        <PeopleIcon />
+      </ListItemIcon>
+      <ListItemText primary="Customers" />
+    </ListItem>
+    <ListItem button>
+      <ListItemIcon>
+        <BarChartIcon />
+      </ListItemIcon>
+      <ListItemText primary="Reports" />
+    </ListItem>
+    <ListItem button>
+      <ListItemIcon>
+        <LayersIcon />
+      </ListItemIcon>
+      <ListItemText primary="Integrations" />
+    </ListItem>
+  </div>
+);
