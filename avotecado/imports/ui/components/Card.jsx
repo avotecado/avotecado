8ac4244742dae9b77e8cbae89feab721268cff41
0x73006b <khan.k.shabab@gmail.com>
2019-07-03 21:56:29 -0700

    just some more router stuff

--- avotecado/imports/ui/components/Card.jsx
+++ avotecado/imports/ui/components/Card.jsx
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import PolBar from './PolBar'
-import BioPage from './BioPage'
+import PolBar from './PolBar';
+import BioPage from './BioPage';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 
-function Card(props) {
-  console.log("Selected passed to card: ", props.selectedPolitician);
+function Card (props) {
+  console.log('Selected passed to card: ', props.selectedPolitician);
   return (
     <React.Fragment>
       <CssBaseline />
-      <Container maxWidth="lg" >
-        <Typography component="div" style={{ backgroundColor: '#cfe8fc' }}>
+      <Container maxWidth='lg' >
+        <Typography component='div' style={{ backgroundColor: '#cfe8fc' }}>
           {/* <div className="card"> */}
           <PolBar />
           {/* <PolBar firstname={props.selectedPolitician.firstname} lastname={props.selectedPolitician.lastname} party={props.selectedPolitician.party} /> */}
@@ -41,7 +41,7 @@ const mapStateToProps = (state) => {
   return {
     politicians: state.politicianListReducer,
     selectedPolitician: state.selectPoliticianReducer
-  }
-}
+  };
+};
 // export default Card;
-export default connect(mapStateToProps)(Card); 
+export default connect(mapStateToProps)(Card);
--- avotecado/imports/ui/components/Home.jsx
+++ avotecado/imports/ui/components/Home.jsx
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
 
 class Home extends Component {
   render () {
     return (
-      <div className='content' style={{ color: 'white' }}>
-        Welcome to avotecado.
-          We provide an easy to access method for everyday people with busy lives to get quickly informed about local politics.
-          Our current offerings are for Vancouver.
+      <div>
+        <CssBaseline />
+        <Container maxWidth='md' >
+          <Typography component='div' style={{ backgroundColor: '#cfe8fc' }}>
+            Welcome to avotecado.
+              We provide an easy to access method for everyday people with busy lives to get quickly informed about local politics.
+              Our current offerings are for Vancouver.
 
-        Why?
-          Understanding politics at every level is important. There is a high ddegree of apathy in the local when compared to provincial and federal level participation.
+            Why?
+              Understanding politics at every level is important. There is a high ddegree of apathy in the local when compared to provincial and federal level participation.
+          </Typography>
+        </Container>
       </div>
     );
   }
