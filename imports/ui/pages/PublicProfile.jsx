a9a1e8ed42bed2f8f6f6e031cf053c12e261371e
0x73006b <khan.k.shabab@gmail.com>
2019-08-03 00:37:42 -0700

    got user data reading in @ cdm, cdu for public profile, should rename+refactor some files

--- imports/ui/pages/PublicProfile.jsx
+++ imports/ui/pages/PublicProfile.jsx
@@ -3,21 +3,42 @@ import {withTracker} from "meteor/react-meteor-data";
 import {Meteor} from "meteor/meteor";
 
 class PublicProfile extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            loading: true
+        }
+    }
+
+    componentDidMount() {
+        console.log(this.props);
+        let userId = this.props.location.search.replace('?', '');
+        console.log(userId);
+        let user = (Meteor.users.find({_id: userId}).fetch());
+        this.setState({loading: false, user: user});
+    }
+
     componentDidUpdate(prevProps, prevState) {
         if (prevProps !== this.props) {
             console.log(this.props);
             let userId = this.props.location.search.replace('?', '');
             console.log(userId);
-            console.log(Meteor.users.find({_id: userId}).fetch());
+            let user = (Meteor.users.find({_id: userId}).fetch());
+            this.setState({loading: false, user: user});
         }
     }
 
     render() {
-        return (
-            <div>
-                ()
-            </div>
-        )
+        if (this.state.loading) {
+            return (<>Loading...</>);
+        } else {
+            let user = JSON.stringify(this.state.user);
+            return (
+                <div>
+                    {user}
+                </div>
+            );
+        }
     }
 }
 
